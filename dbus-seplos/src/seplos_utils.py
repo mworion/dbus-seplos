cdac055212beb86ab20daca349af53960f1cd728
mworion <michael@wuertenberger.org>
2024-03-17 17:49:34 +0100

    go for 0.1.4

--- dbus-seplos/src/seplos_utils.py
+++ dbus-seplos/src/seplos_utils.py
@@ -25,7 +25,7 @@ import logging
 import time
 import datetime
 
-DRIVER_VERSION = '0.1.2'
+DRIVER_VERSION = '0.1.4'
 
 logging.Formatter.converter = time.gmtime
 timeTag = datetime.datetime.utcnow().strftime('%Y-%m-%d')
