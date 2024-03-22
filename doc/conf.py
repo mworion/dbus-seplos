d15508fddd9dd778095bc49d68c449d58c8c0d77
mworion <michael@wuertenberger.org>
2024-03-22 18:48:58 +0100

    make doc copy button permanent

--- doc/conf.py
+++ doc/conf.py
@@ -79,6 +79,7 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ["custom.css"]
 
 pdf_documents = [('index', u'dbus-seplos', u'dbus-seplos', u'mworion')]
 # index - master document
